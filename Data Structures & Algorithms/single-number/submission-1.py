4a37298af1be9d5c2a79dd60adeaa6328b27ce4b
SinghRavinder-97 <ravinder.singh8g3@gmail.com>
2026-06-07 04:14:01 -0400

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        checkstack = []
+        for i in nums:
+            if i in checkstack:
+                checkstack.remove(i)
+            else:
+                checkstack.append(i)        
+        return checkstack[-1]
+        
